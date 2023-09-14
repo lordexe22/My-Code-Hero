affa175bca3f54c0baa81982af33d5b047b29c2a
lordexe22 <walterezequielpuig@gmail.com>
2023-09-13 22:50:07 -0300

    Remodela la sección react-hook-form/introduccion

--- src/pages/React-Hook-Form/Introduccion_ReactHookForm.page.jsx
+++ src/pages/React-Hook-Form/Introduccion_ReactHookForm.page.jsx
@@ -3,21 +3,22 @@ export default function IntroduccionReactHookForm(){
 
     return(
         <>
-            <h3>Introduccion a React Hook Form</h3>
+            <h3>Introducción a React Hook Form</h3>
 
             <h4>¿Qué es React Hook Form?</h4>
 
             <p>React Hook Form es una biblioteca de gestión de formularios en React que simplifica considerablemente la creación y el manejo de formularios en las aplicaciones.</p>
 
-            <h4>Ventajas de usar React Hook Form</h4>
+            <h4>¿Por qué usar React Hook Form?</h4>
 
             <p>La gestión de formularios puede ser una tarea complicada en cualquier aplicación web. React Hook Form aborda este desafío al proporcionar una forma más sencilla y declarativa de construir y validar formularios. Algunas de las ventajas clave de usar React Hook Form incluyen:</p>
 
             <ul>
-                <li><b>Estructura basada en Hooks:</b> Utiliza los hooks de React, lo que significa que puedes aprovechar las características más modernas de React para gestionar tus formularios.</li>
-                <li><b>Optimización de Rendimiento:</b> React Hook Form minimiza las re-renderizaciones innecesarias, lo que resulta en un mejor rendimiento de la aplicación.</li>
-                <li><b>Validación de Formulario Integrada:</b> Ofrece una sólida validación de formularios con reglas de validación personalizables, lo que garantiza que tus datos sean correctos y seguros.</li>
-                <li><b>Gestión de Estados Simplificada: </b>Facilita el seguimiento y la gestión del estado de tus formularios, sin necesidad de crear un montón de estados locales.</li>
+                <li>Permite la administración de los datos del formulario.</li>
+                <li>Permite el envio de los datos del formulario.</li>
+                <li>Hace cumplir las validaciones de los campos del formulario.</li>
+                <li>Proporciona feddback visual.</li>
+                <li>Proporciona una forma simple y escalable de manejar hasta los formularios más complejos.</li>
             </ul>
         </>
     )

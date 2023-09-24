5aeafa58540281f1205122d199b15b420a870b7a
lordexe22 <walterezequielpuig@gmail.com>
2023-09-23 21:53:29 -0300

    Se agrego la seccion express/introduccion

--- src/pages/Express/Introduccion_express.page.jsx
+++ src/pages/Express/Introduccion_express.page.jsx
@@ -4,6 +4,16 @@ export default function IntroduccionExpress(){
     return(
         <>
             <h3>Introduccion a Express</h3>
+
+            <p>Express es un framework web rápido, flexible y minimalista para <b>Node.js</b>. Es ampliamente utilizado para construir aplicaciones web y servicios API de forma sencilla y eficiente. No impone una estructura de proyecto rígida, lo que permite a los desarrolladores organizar su código de la manera que prefieran.</p>
+
+            <p>Posee la capacidad de integrarse con una amplia variedad de módulos adicionales, lo que permite agregar funcionalidades específicas según las necesidades del proyecto. Estos módulos, conocidos como middleware, se utilizan para realizar tareas como el manejo de sesiones, la autenticación de usuarios, la compresión de datos, el enrutamiento avanzado, entre otros.</p>
+
+            <p>Express proporciona una capa de abstracción sobre las funcionalidades básicas de <b>Node.js</b>, lo que simplifica el manejo de rutas, solicitudes HTTP, respuestas y middleware. Permite crear fácilmente endpoints para manejar diferentes tipos de solicitudes, como GET, POST, PUT y DELETE, y responder con datos en formato JSON, HTML y XML entre otros.</p>
+
+
+
+
        </>
     )
 }

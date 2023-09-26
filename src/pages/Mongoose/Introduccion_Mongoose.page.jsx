a69a8f2c11262d27b5816fd9943079d43cbc832b
lordexe22 <walterezequielpuig@gmail.com>
2023-09-26 12:22:27 -0300

    Se añade la seccion mongoose/introduccion

--- src/pages/Mongoose/Introduccion_Mongoose.page.jsx
+++ src/pages/Mongoose/Introduccion_Mongoose.page.jsx
@@ -4,6 +4,25 @@ export default function IntroduccionMongoose(){
     return(
         <>
             <h3>Introduccion a Mongoose</h3>
+
+            <p>La combinación de Node.js y MongoDB ha transformado la forma en que desarrollamos aplicaciones web y móviles. Node.js ofrece un entorno de ejecución rápido y eficiente, mientras que MongoDB es una base de datos NoSQL altamente flexible que almacena datos en documentos BSON (Binary JSON). Sin embargo, para aprovechar al máximo MongoDB en el mundo Node.js, necesitas una herramienta que simplifique la interacción y agregue estructura. Aquí es donde Mongoose entra en escena.</p>
+
+            <p>Mongoose es un ODM (Mapeo Objeto-Documento) de Node.js que actúa como un intermediario inteligente entre la aplicación de Node.js y MongoDB. Esta biblioteca simplifica la interacción con MongoDB al proporcionar una capa de abstracción que permite definir modelos de datos con esquemas. Los esquemas son como un plano maestro que describe la estructura de tus datos.</p>
+
+            <p>Mongoose se convierte en un aliado valioso para desarrolladores Node.js que trabajan con MongoDB. Ya sea creando una aplicación web, una API REST o cualquier proyecto basado en Node.js, esta biblioteca simplifica tareas al proporcionar un marco sólido para interactuar con MongoDB.</p>
+
+            <h4>Controls y validación de datos</h4>
+
+            <p>Un aspecto esencial de Mongoose es su capacidad para definir esquemas que especifican la estructura de tus datos. Puedes definir qué campos son requeridos, el tipo de datos que deben contener y reglas de validación personalizadas. Esto asegura que los datos almacenados en MongoDB cumplan con ciertos criterios, lo que es fundamental para mantener la integridad de tu base de datos.</p>
+
+            <h4>Personalización</h4>
+
+            <p>Mongoose ofrece una amplia gama de características adicionales que facilitan tareas comunes de la base de datos. Puedes ejecutar consultas avanzadas, aplicar ganchos (hooks) para personalizar el comportamiento de tu aplicación y usar middleware para automatizar procesos.</p>
+
+            <h4>Desarrollo lfexible</h4>
+
+            <p>Las aplicaciones a menudo evolucionan con el tiempo, y es probable que debas realizar cambios en la estructura de tu base de datos. Mongoose facilita este proceso, permitiéndote realizar modificaciones en el esquema de datos de manera controlada y sencilla. Esto es fundamental en el desarrollo ágil y dinámico.</p>
+
         </>
     )
 }
